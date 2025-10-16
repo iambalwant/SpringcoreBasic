3363bf11400656e5aa2c198589c25371bb11b9e5
iambalwant <balwantverma55555@gmail.com>
2025-10-17 01:47:55 +0530

    AutoWiring @Qualifier annotation

--- src/main/java/com/springcore/autowiringAnn/Employee.java
+++ src/main/java/com/springcore/autowiringAnn/Employee.java
@@ -2,11 +2,13 @@ package com.springcore.autowiringAnn;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
 
 //    we have to inject address class into employee class - employee is dependent on address class
     @Autowired //only this annotation need to autowire - uses type to autowire
+    @Qualifier("address2") //if we want to specify bean by name
     private Address address;
 
     public Address getAddress() {
